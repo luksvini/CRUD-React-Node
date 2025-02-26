856be280252982f456e520be9b1c2debc08b7026
Lucas <vinlucaz9@gmail.com>
2025-02-26 11:07:29 -0300

    ajustes env

--- back-end/api/supabase.js
+++ back-end/api/supabase.js
@@ -6,18 +6,18 @@ import { createClient } from '@supabase/supabase-js';
 dotenv.config();
 
 // Verificar se as variáveis estão sendo carregadas corretamente
-console.log("SUPABASE_URL:", process.env.SUPABASE_URL);
-console.log("SUPABASE_ANON_KEY:", process.env.SUPABASE_ANON_KEY);
+console.log("SUPABASE_URL:", process.env.EXPO_PUBLIC_SUPABASE_URL);
+console.log("SUPABASE_ANON_KEY:", process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY);
 
 // Garantir que as variáveis foram carregadas
-if (!process.env.SUPABASE_URL || !process.env.SUPABASE_ANON_KEY) {
+if (!process.env.EXPO_PUBLIC_SUPABASE_URL || !process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY) {
   throw new Error('Variáveis de ambiente do Supabase não foram carregadas corretamente.');
 }
 
 // Criar cliente do Supabase
 const supabase = createClient(
-  process.env.SUPABASE_URL, 
-  process.env.SUPABASE_ANON_KEY
+  process.env.EXPO_PUBLIC_SUPABASE_URL, 
+  process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY
 );
 
 export default supabase;
